558a04399d6fdc28705d1fc69efbf08846bc95d7
EdigleisonBr <edigleisonbr@gmail.com>
2022-03-18 16:17:37 -0300

    first commit

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,13 @@
+<aside class="left-sidebar" data-sidebarbg="skin5">
+    <!-- Sidebar scroll-->
+    <div class="scroll-sidebar">
+        <!-- Sidebar navigation-->
+        <nav class="sidebar-nav">
+            <ul id="sidebarnav" class="pt-4">
+                @include('layouts.menu')
+            </ul>
+        </nav>
+        <!-- End Sidebar navigation -->
+    </div>
+    <!-- End Sidebar scroll-->
+</aside>
